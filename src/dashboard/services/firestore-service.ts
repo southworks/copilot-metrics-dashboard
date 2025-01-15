3f9206d1e364d972351cae29c3574e4347d4b298
Elian Temes Sanchez <elian.temes@southworks.com>
2025-01-15 17:17:09 -0300

    removed secret manager referencing

--- src/dashboard/services/firestore-service.ts
+++ src/dashboard/services/firestore-service.ts
@@ -1,7 +1,6 @@
 import { initializeApp } from 'firebase/app';
-import { getFirestore, Firestore, collection } from 'firebase/firestore';
+import { getFirestore, Firestore } from 'firebase/firestore';
 import { stringIsNullOrEmpty } from "../utils/helpers";
-import { getSecret } from './secrets-service';
 
 type firebaseConfig = {
   apiKey: string,
@@ -14,9 +13,9 @@ type firebaseConfig = {
 
 const getFirebaseConfig = async (): Promise<firebaseConfig> => { 
   return {
-    apiKey: await getSecret(process.env.FIREBASE_API_KEY_SECRET_NAME || ''),
+    apiKey: process.env.FIREBASE_API_KEY || '',
     authDomain: process.env.FIREBASE_AUTH_DOMAIN || '',
-    projectId: process.env.FIREBASE_PROJECT_ID || '',
+    projectId: process.env.PROJECT_ID || '',
     storageBucket: process.env.FIREBASE_STORAGE_BUCKET || '',
     messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID || '',
     appId: process.env.FIREBASE_APP_ID || '',
@@ -27,7 +26,7 @@ const app = initializeApp(await getFirebaseConfig());
 const db = getFirestore(app, process.env.FIRESTORE_DATABASE_ID || '');
 
 export const firestoreClient = (): Firestore => {
-  const projectId = process.env.FIREBASE_PROJECT_ID;
+  const projectId = process.env.PROJECT_ID;
 
   if (stringIsNullOrEmpty(projectId)) {
     throw new Error("Missing required environment variable for Firestore project ID");
