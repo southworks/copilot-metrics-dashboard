fc0d500cb401d1882213c46c2f2527712bb03e50
Elian Temes Sanchez <elian.temes@southworks.com>
2025-01-21 15:45:46 -0300

    Added tooltips to team dashboard

--- src/dashboard/features/dashboard/charts/team-stats.tsx
+++ src/dashboard/features/dashboard/charts/team-stats.tsx
@@ -19,16 +19,19 @@ export const Stats = () => {
         title="Acceptance average"
         description="Inline code acceptance average"
         value={acceptanceAverage.toFixed(2) + "%"}
+        tooltipContent="The acceptance average is calculated as the cumulative ratio of total lines accepted to total lines suggested, multiplied by 100."
       ></StatsCard>
       <StatsCard
         title="Active users"
         description="Average active users"
         value={averageActiveUsers.toFixed(0) + ""}
+        tooltipContent="The average number of active users is calculated by averaging the total active users over the selected time frame."
       ></StatsCard>
       <StatsCard
         title="Engaged users"
-        description="Average engaged users"
+        description="Average engaged users."
         value={averageEngagedUsers.toFixed(0) + ""}
+        tooltipContent="Engaged users are calculated by averaging the total number of engaged users across all data points. An engaged user is someone who has actively interacted with the system during the specified time frame."
       ></StatsCard>
     </div>
   );
